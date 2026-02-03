c65b684e04e0b9b15deb9a920c78694f4c7228ff
Omar Carrillo <ocarrillo@Mac-mini-de-Omar.local>
2026-02-02 21:58:49 -0600

    update: variables.tf modifications

--- xc/variables.tf
+++ xc/variables.tf
@@ -292,7 +292,7 @@ variable "azure_service_principal_password" {
 
 variable "azure_xc_machine_type" {
   type    = string
-  default = "Standard_D4s_v3"
+  default = "Standard_D4s_v34"
 }
 
 # XC Service Discovery
