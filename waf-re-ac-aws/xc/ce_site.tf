181e0884fdfe72c4ada50d7a175560f90f4b39bb
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-03-18 08:41:55 -0500

    fix: force null_resource re-execution on every apply with timestamp trigger

--- waf-re-ac-aws/xc/ce_site.tf
+++ waf-re-ac-aws/xc/ce_site.tf
@@ -47,6 +47,10 @@ resource "volterra_aws_vpc_site" "ce" {
 resource "null_resource" "wait_for_ce_validation" {
   depends_on = [volterra_aws_vpc_site.ce]
 
+  triggers = {
+    always_run = timestamp()
+  }
+
   provisioner "local-exec" {
     command = "${path.module}/check_ce_status.sh ${var.api_url}/api/register/namespaces/system/site/${volterra_aws_vpc_site.ce.name} ${path.module}/api.p12 '' 600 cert $VES_P12_PASSWORD"
   }
