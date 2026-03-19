8afc189b088ab2fa199ffd9cde60f4d8efe9f248
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-03-19 18:34:05 -0500

    fix: change EKS node group to t3.large to stay within 16 vCPU limit
    3 CE masters (t3.xlarge) = 12 vCPU + 1 EKS node (t3.large) = 2 vCPU = 14 total
    Previous: 3x4 + 1x4 = 16 vCPU = exactly at limit, causing VcpuLimitExceeded

--- waf-ce-k8s-aws/eks-cluster/eks_cluster.tf
+++ waf-ce-k8s-aws/eks-cluster/eks_cluster.tf
@@ -25,7 +25,7 @@ resource "aws_eks_node_group" "private-node-group-1-tf" {
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids = var.skip_private_subnet_creation ? [for i in aws_subnet.eks-external: i.id] : [for i in aws_subnet.eks-internal: i.id]
   disk_size = 30
-  instance_types = ["t3.xlarge"]
+  instance_types = ["t3.large"]
 
   scaling_config {
    desired_size = var.desired_size
