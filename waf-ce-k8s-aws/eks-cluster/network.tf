0f61709543a9c7f677a3c545daf9de253d5bf916
Omar Carrillo <ocarrillo@MacBook-Air-de-Omar.local>
2026-03-19 08:50:30 -0500

    fix: replace deprecated vpc=true with domain=vpc in aws_eip, upgrade VPC module to v5

--- waf-ce-k8s-aws/eks-cluster/network.tf
+++ waf-ce-k8s-aws/eks-cluster/network.tf
@@ -2,7 +2,7 @@
 
 # Create Elastic IP
 resource "aws_eip" "main" {
-  vpc              = true
+  domain           = "vpc"
   tags = {
     resource_owner = local.resource_owner
     Name          = format("%s-eip-%s", local.project_prefix, local.build_suffix)
